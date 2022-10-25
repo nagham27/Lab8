b681f6ad9270703560271ffbc813a7ff8c2bbe5b
Nagham Eldegwy <naghamhesham27@gmail.com>
2022-10-25 17:25:02 -0600

    add tests to CustomListTest

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,2 +1,34 @@
-package com.example.lab8;public class CustomListTest {
+package com.example.lab8;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+
+    private CustomList list;
+    /**
+     * we create a CustomList object before running
+     */
+    @Before
+    public void createList(){
+        list = new CustomList(null,new ArrayList<City>());
+    }
+
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size plus
+     one
+     */
+    @Test
+    public void addCityTest(){
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
+
 }
